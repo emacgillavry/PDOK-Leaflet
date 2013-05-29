4134bcb51f6928273e680bf63dfc59f1d15df422
Edward Mac Gillavry <emacgillavry@gmail.com>
2013-05-29 10:21:06 +0200

    Adjusted view to Groningen and removed RD-coordinate test.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// Resoluties (pixels per meter) van de zoomniveaus:
 var res = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420];
+
+// Juiste projectieparameters voor Rijksdriehoekstelsel (EPSG:28992):
 var RD = L.CRS.proj4js('EPSG:28992', '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs', new L.Transformation(1, 285401.920, -1, 903401.920));
 RD.scale = function(zoom) {
     return 1 / res[zoom];
@@ -15,13 +18,6 @@ var map = new L.Map('map', {
         continuousWorld: true
     })
   ],
-  center: new L.LatLng(52, 5.3),
-  zoom: 3
-});
-// test RD coordinates
-map.on('click', function(e) {
-    if (window.console) {
-        var point = RD.projection.project(e.latlng);
-        console.log("RD X: " + point.x + ", Y: " + point.y);
-    }
+  center: new L.LatLng(53.219231,6.57537),
+  zoom: 7
 });
